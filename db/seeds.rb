4f0f30611a5dddd3e56140b785f4a99c0507eec3
Basile Sowka <basile.sowka@gmail.com>
2019-05-22 15:52:11 +0200

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'open-uri'
+require 'json'
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 user_serialized = open(url).read
